0c014487bb25fab807fe536f79bf667eb863732b
eli-lapkin <Eli@Elis-MacBook.home>
2017-05-23 18:23:52 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { TodoListHeaderComponent } from './todo-list-header/todo-list-header.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoListItemComponent } from './todo-list-item/todo-list-item.component';
+import { TodoListFooterComponent } from './todo-list-footer/todo-list-footer.component';
+import { TodoDataService } from './todo-data.service';
+
 import { AppComponent } from './app.component';
+import { ListsComponent } from './lists/lists.component';
+import { HomeComponent } from './home/home.component';
+
+import { routes } from './app.router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListHeaderComponent,
+    TodoListComponent,
+    TodoListItemComponent,
+    TodoListFooterComponent,
+    ListsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routes
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ TodoDataService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/app.router.ts
@@ -0,0 +1,14 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ListsComponent } from './lists/lists.component';
+
+export const router: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'lists', component: ListsComponent },
+];
+
+export const routes: ModuleWithProviders = RouterModule.forRoot(router);
